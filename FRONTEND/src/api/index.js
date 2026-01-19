32094bf15a86e2628f519dcedb472852b2225b92
Moro383838 <mohammadkhiarah@gmail.com>
2026-01-19 09:08:41 +0200

    grid

--- FRONTEND/src/api/index.js
+++ FRONTEND/src/api/index.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: 'https://considerate-miracle-production-42bb.up.railway.app/api/v1'
   , timeout: 10000
-})
+})  
 
 api.interceptors.request.use(
   (config) => {
